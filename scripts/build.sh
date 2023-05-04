0fbf4e4e2b3cb1b63d50c6399e597bd2b30c829f
David Zhang <crispgm@gmail.com>
2023-05-04 13:17:04 +0800

    fix: css path

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ mkdir docs
 cp -r src/* docs/
 
 sed -i 's/src="\/app.js"/src="\/referee\/app.js"/g' docs/index.html
-sed -i 's/<link href="\/assets\/style.css" rel="stylesheet">/<link href="\/referee\/assets\/style.css" rel="stylesheet">/g' docs/index.html
+sed -i 's/<link href="\/assets\/css\/style.css" rel="stylesheet">/<link href="\/referee\/assets\/css\/style.css" rel="stylesheet">/g' docs/index.html
