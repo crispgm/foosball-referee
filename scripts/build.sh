d388d6d1ec6f97196cadcf6272f4d96976c9b595
David Zhang <crispgm@gmail.com>
2023-05-04 14:03:52 +0800

    fix: button color

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,6 @@
 
 mkdir docs
 cp -r src/* docs/
-cp -r assets docs/
 
 sed -i 's/src="\/app.js"/src="\/referee\/app.js"/g' docs/index.html
 sed -i 's/<link href="\/assets\/css\/style.css" rel="stylesheet">/<link href="\/referee\/assets\/css\/style.css" rel="stylesheet">/g' docs/index.html
