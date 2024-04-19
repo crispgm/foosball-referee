0d62d8ee53354ef0d86894771542db27b6f571ec
David Zhang <crispgm@gmail.com>
2024-04-19 11:30:47 +0800

    fix: release path

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,5 @@ cp -r src/* docs/
 sed -i 's/src="\/app.js"/src="\/foosball-referee\/app.js"/g' docs/index.html
 sed -i 's/<link href="\/assets\/css\/style.css" rel="stylesheet">/<link href="\/foosball-referee\/assets\/css\/style.css" rel="stylesheet">/g' docs/index.html
 sed -i 's/<link type=\"image\/x-icon\" href="\/assets\/images\/icon.png" rel="icon">/<link type=\"image\/x-icon\" href="\/foosball-referee\/assets\/images\/icon.png" rel="icon">/g' docs/index.html
+sed -i 's/\/assets\/css/\/foosball-referee\/assets\/css/g' docs/assets/css/style.css
+sed -i 's/\/assets\/css/\/foosball-referee\/assets\/css/g' docs/assets/css/themes/themes.css
